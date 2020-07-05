0564077b069ca2fa3f37ca1398bb2ce3f691e440
dylan <dylan_zeng92@qq.com>
2020-07-05 23:57:00 +0800

    todo: lengthOfLIS

---
+++ src/medium/lengthOfLIS.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+  const len = nums.length
+  if (len < 1) return len
+  let dp = Array.from(new Array(len), i => new Array(len).fill(0))
+  let max = 0
+  for(let i = len -1; i >= 0; i --) {
+      for(let j = i; j < len -1 ; j ++) {
+          if ((j - i < 1) dp[j][i] = true
+          else {
+              console.log(j +1, i -1)
+              dp[j][i] = dp[j + 1][i -1] && nums[j] < nums[j + 1] && nums[i] > nums[i-1]
+          }
+          console.log(dp[j][i], j, i, nums[j], nums[i])
+          if (dp[j][i]) {
+              max = Math.max(max, i - j + 1)
+          }
+      }
+  }
+  return max
+};
+/**
+ * @thought
+ * [list] <s> --- <e>
+ * <s> l     <l-1
+ * <e> r     r > [r-1]
+ * <s+1> <e-1>也是升序的
+ * <s><e>升序的条件 = <s+1><e-1> s < s + 1   <e> > e - 1
+ * <s><e>       <s +1 > <e>  s< s +1 || <s> <e-1> e > e -1
+ * 
+ * i -- j ++
+ */
